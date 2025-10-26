02b9c4e05d17b9ce7eca2a339d9ac43293d59cc6
Miguel Arismendi <meavthelast@gmail.com>
2025-10-26 17:28:38 -0400

    a chambear

---
+++ templates/navbar.html.php
@@ -0,0 +1,4 @@
+<?php
+//para no confundirnos los archivo que sean plantillas reutilizables y paginas (en su mayoria html) van a llevar .html.php
+// tecnicamente siguen siendo archivos .php porque es la ultima extension pero le ponemos .html para no confundirnos, si fuera 
+// mas que todo logica y php pelao ahi si llevaria solamente .php
