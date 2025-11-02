186a7c3e3bd3ea2b8c41520de906390b25792069
Miguel Arismendi <meavthelast@gmail.com>
2025-11-02 02:48:26 -0400

    arreglado el navbar, el hover y un poco de la esturctura del index

--- index.php
+++ index.php
@@ -7,28 +7,29 @@
     <link rel="stylesheet" href="styles/bootstrap5.css">
     <link rel="stylesheet" href="styles/styles.css">
 </head>
-<body background="templates/Images/fondo.svg">
-    <?php include 'templates/navbar.html.php'; ?>
+<body  style="background-image: url(templates/Images/fondo.svg); background-size:auto;">
+    <img class="position-absolute img-fluid" style="width: 100%; padding: 15% 0 ; z-index: 0;" src="media/banner1.png" alt="banner" >
+    <?php require 'templates/navbar.html.php'; ?>
     <!-- Div General (Para las imagenes) -->
     <div class="container-fluid mt-4 p-0 m-0">
-        <img src="templates/Images/letras.svg" alt="" class="float-start">
-        <img src="templates/Images/letras2.svg" class="float-end" alt="" class="float-end">
+        <img style="z-index: -1;" src="templates/Images/letras.svg" alt="" class="float-start position-absolute">
+        <img style="z-index: -1;" src="templates/Images/letras2.svg" class="float-end position-absolute" alt="" class="float-end">
         <!-- Div Para el apartado de elige un Dojo -->
-        <div class="container pt-5 pb-5">
-            <h1 class="text-center text-white">Elige un Dojo</h1>
-            <div class="row justify-content-center">
-                <div class="col-12 col-md-4 mb-3 d-flex justify-content-center">
-                    <img src="templates/Images/dojo3.png" alt="Logo Cobra Kai" class="Dojo img-fluid p-2" style="max-width: 300px;">
-                    <img src="templates/Images/dojo2.png" alt="Logo Eagle Fang Karate" class="Dojo img-fluid p-2" style="max-width: 300px;">
-                    <img src="templates/Images/dojo1.png" alt="Logo Miyagi-Do Karate" class="Dojo img-fluid p-2" style="max-width: 300px;">
-                </div>
+        <div id="espacio_vacio" style="padding-top: 900px; position: relative; z-index: 1;"></div>
+        <div class="container pt-5 pb-5" >
+            <h1 class="text-center text-warning dead-stock titulo mb-5">Elige un Dojo</h1>
+            <div class="row justify-content-center gx-5">
+                <div class="col-2 Dojo Dojo1 img-fluid m-4" style="z-index: 1;"></div>
+                <div class="col-2 Dojo Dojo2 img-fluid m-4" style="z-index: 1;"></div>
+                <div class="col-2 Dojo Dojo3 img-fluid m-4" style="z-index: 1;"></div>
             </div>
         </div>
     </div>
     <!-- Div Primera Temporada -->
-    <div>
-        <h1>Mira la Primera Temporada</h1>
-        <button>Ver Ahora</button>
+    <div id="espacio_vacio" style="padding-top: 300px; position: relative; z-index: 1;"></div>
+    <div class="container-fluid d-flex flex-column align-items-center justify-content-center" style="background-image: url(media/johnny_amarillo.png); height: 700px; background-size: cover; background-repeat: no-repeat; width: 100vw; background-position: center;">
+        <div class="row"><div class="col-12 text-center"><p class="text-white text-center dead-stock fs-1">Mira la Primera Temporada</p></div></div>
+        <div class="row"><div class="col position-relative"><button>Ver ahora</button></div></div>
     </div>
     <!-- Div Merch -->
      <div>
