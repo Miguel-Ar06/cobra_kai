3ba6e77a18e8011f4efb38c3171688ad480cbb31
GabrielViorUni <gvivas.9287@unimar.edu.ve>
2025-11-02 20:16:13 -0400

    Karate Kid

--- karate_kid.php
+++ karate_kid.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="styles/bootstrap5.css">
     <link rel="stylesheet" href="styles/styles.css">
 </head>
-<body style="background-image: url(templates/Images/fondo.svg); background-size:auto;">
+<body>
     <img src="media/karate_kidBackground.png" alt="banner" class="position-absolute img-fluid" style="width: 100%; padding-top: 15% ; z-index: -1;" >
     <?php require 'templates/navbar.html.php'; ?>
     <!-- Karate Kid -->
@@ -25,9 +25,70 @@
     </div >
     <div class="container-fluid d-flex flex-column align-items-center justify-content-center text-justify" style="background-image: url(media/FondoMuchachito.png);">
         <div>
-            <p>Es la primera película de la franquicia que sigue la historia de Daniel LaRusso interpretado por Ralph Macchio un adolescente que se muda con su viuda al barrio de Reseda de Los Ángeles. Allí, LaRusso se enfrenta al acoso de matones, uno de los cuales es Johnny Lawrence quien es interpretado por William Zabka.</p>
+            <p class="fs-4 text-white" style="padding-top:7%; padding-left: 8%; width: 47%; background-repeat: no-repeat; background-size: cover;">Es la primera película de la franquicia que sigue la historia de Daniel LaRusso interpretado por Ralph Macchio un adolescente que se muda con su viuda al barrio de Reseda de Los Ángeles. Allí, LaRusso se enfrenta al acoso de matones, uno de los cuales es Johnny Lawrence quien es interpretado por William Zabka. <br><br>
+            Un veterano de guerra llamado el Sr. Miyagi se ofrece a entrenar a Daniel para defenderse de los matones y participar en el torneo de All Valey.
+        </p>
+        <button class="boton-amarillo" style="margin-left:8%;">Ver Ahora</button>
         </div>
     </div>
-    
+    <!-- Segunda Parte de mi sufrimiento -->
+    <div class="container-fluid d-flex flex-column align-items-center justify-content-center text-justify" style="background-image: url(media/PersonasChinas.png); padding-top: 5%; padding-bottom:5%;" >
+        <h1 class="text-center text-white dead-stock titulo mb-5">
+            The Karate Kid Parte II
+        </h1>
+    </div>
+    <div class="container-fluid d-flex flex-column align-items-center justify-content-center text-justify" style="background-image: url(media/OtroKarateka.png);">
+        <div>
+            <p class="fs-3 text-black" style="padding-top:7%; margin-left: 65%; width: 25%; background-repeat: no-repeat; background-size: cover; text-align: right;">
+                En esta secuela Daniel junto al Sr. Miyagi viaja a la tierra natal del Sensei en Okinawa. y allí aprende cosas sobre el pasado secreto de su maestro. Además, el regreso de Miyagi reavivará su vieja enemistad con Sato y arrastrará a Daniel a un choque de culturas y combates.
+        </p>
+        <button class="boton-amarillo" style="margin-left:75%;">Ver Ahora</button>
+        </div>
+    </div>
+    <!-- Tercera Parte de mi sufrimiento -->
+     <div class="container-fluid d-flex flex-column align-items-center justify-content-center text-justify" style="background-image: url(media/img1984.png); padding-top: 10%; padding-bottom:10%;" >
+            <h1 class="text-center text-white dead-stock titulo mb-5">
+                The Karate Kid Part III
+            </h1>
+    </div>
+    <div class="container-fluid d-flex flex-column align-items-center justify-content-center text-justify" style="background-image: url(media/TercerKarateka.png);">
+        <div>
+            <p class="fs-4 text-white" style="padding-top:7%; padding-left: 8%; width: 28%; background-repeat: no-repeat; background-size: cover;">
+                En Okinawa Daniel y el Sr. Miyagi se dedican a trabajar en una tienda de bonsáis, sin embargo, su paz se ve truncada con la aparición de Mike Baners, un despiadado maestro de karate que amenaza la filosofía de las artes marciales. <br><br>Miyagi se niega a enfrentarlo y desesperado LaRusso busca a otro mentor.
+        </p>
+        <button class="boton-amarillo" style="margin-left:8%;">Ver Ahora</button>
+        </div>
+    </div>
+    <!-- Cuarta Parte de mi sufrimiento -->
+     <div class="container-fluid d-flex flex-column align-items-center justify-content-center text-justify" style="background-image: url(media/PersonasChinas.png); padding-top: 5%; padding-bottom:5%;" >
+        <h1 class="text-center text-white dead-stock titulo mb-5">
+            The Karate Kid Part 4
+        </h1>
+    </div>
+    <div class="container-fluid d-flex flex-column align-items-center justify-content-center text-justify" style="background-image: url(media/Chamaquita.png);">
+        <div>
+            <p class="fs-3 text-black" style="padding-top:7%; margin-left: 65%; width: 25%; background-repeat: no-repeat; background-size: cover; text-align: right;">
+                El Sr Miyagi estaba en una conmemoración a los soldados japoneses que lucharon en la segunda guerra mundial, cuando conoce a Julie una adolescente huérfana con problemas de ira, y con el objetivo de ayudarla decide enseñarle artes marciales.
+        </p>
+        <button class="boton-amarillo" style="margin-left:75%;">Ver Ahora</button>
+        </div>
+    </div>
+    <!-- Quinta Parte de mi sufrimiento -->
+      <div class="container-fluid d-flex flex-column align-items-center justify-content-center text-justify" style="background-image: url(media/img1984.png); padding-top: 10%; padding-bottom:10%;" >
+            <h1 class="text-center text-white dead-stock titulo mb-5">
+                The Karate Kid 2010
+            </h1>
+    </div>
+    <div class="container-fluid d-flex flex-column align-items-center justify-content-center text-justify" style="background-image: url(media/MiniWilliSmi.png);">
+        <div>
+            <p class="fs-4 text-white" style="padding-top:7%; padding-left: 8%; width: 40%; background-repeat: no-repeat; background-size: cover;">
+                <br><br><br>
+                En esta ocasión la historia sigue a Dre Parker un niño se mudó a China. Dre se hace amigo del Sr. Han un obrero en el edificio donde vive quien lo entrena para que aprenda a defenderse. <br><br> Originalmente esta película transcurría en un universo alterno, pero ahora se sabe que pertenece a la misma línea de tiempo del resto de la saga. <br>
+        </p>
+        <button class="boton-amarillo" style="margin-left:8%; margin-bottom:4%">Ver Ahora</button>
+        </div>
+    </div>
+    <br><br><br><br>
+    <?php include 'templates/footer.html.php'; ?>
 </body>
 </html>
