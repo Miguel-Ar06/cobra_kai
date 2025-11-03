960690fec22f57fa69a5319e5861923409b11f13
Sergio-Rojas27 <srojas.1871@unimar.edu.ve>
2025-11-02 21:45:31 -0800

    fideicomiso completo, arreglos menores porai

--- perfil.php
+++ perfil.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Galeria</title>
+    <title>Perfil</title>
     <link rel="stylesheet" href="styles/bootstrap5.css">
     <link rel="stylesheet" href="styles/styles.css">
 </head>
